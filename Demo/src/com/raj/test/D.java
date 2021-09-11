47a6d1748a732d1fb5924305a8bc9bbbe5be9568
AkhilDV <akhil@gmail.com>
2021-09-11 14:38:38 +0530

    done by DEV

--- Demo/src/com/raj/test/D.java
+++ Demo/src/com/raj/test/D.java
@@ -3,15 +3,19 @@ package com.raj.test;
 public class D {
 
 	public static void main(String[] args) {
-		System.out.println("hi");
+		System.out.println(" hi");
 	}
 	public  void add() {
 		System.out.println("adding");
+		int a=5+6;
+		System.out.println(a);
 	}
 	
 	public void sub()
 	{
 		System.out.println("sub");
+		int a=6-5;
+		System.out.println(a);
 	}
 
 
