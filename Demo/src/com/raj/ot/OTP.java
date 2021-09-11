447bbfb4e02060639a56549b31962125a85e411d
AkhilDV <akhil@gmail.com>
2021-09-11 18:10:21 +0530

    stash

--- Demo/src/com/raj/ot/OTP.java
+++ Demo/src/com/raj/ot/OTP.java
@@ -5,10 +5,15 @@ public class OTP {
 	public static void main(String[] args) {
 		System.out.println("otp");
 		System.out.println("otp done");
+		System.out.println("stashing");
 	}
 
-	public void sms()
-	{
+	public void sms() {
 		System.out.println("sms");
 	}
+
+	public void mobile() {
+		System.out.println("enter the number");
+	}
+
 }
